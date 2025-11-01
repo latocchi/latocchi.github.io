c2b4f2d5942950e88a042daa3f95e2a322ae558e
latocchi <jaycybanaga@gmail.com>
2025-11-01 18:16:14 +0800

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "./ui/button";
+
+const Navbar = () => {
+  const navigate = useNavigate();
+  return (
+    <nav className="bg-blue-600 flex justify-center py-2">
+        <ul className="flex items-center justify-center gap-x-2">
+          <li className="border border-black rounded-lg">
+            <Button type="button" onClick={() => navigate("/")}>Home</Button>
+          </li>
+          <li className="border border-black rounded-lg">
+            <Button type="button" onClick={() => navigate("/about")}>About</Button>
+          </li>
+          <li className="border border-black rounded-lg">
+            <Button type="button" onClick={() => navigate("/projects")}>Projects</Button>
+          </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
