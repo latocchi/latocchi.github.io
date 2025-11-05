73609656e946d87789040c46f06316cc98b4df5d
Passenger7390 <jaycybanaga@gmail.com>
2025-11-05 21:30:17 +0800

    fix: horizontal scrollbar no longer showing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 const Navbar = () => {
   const navigate = useNavigate();
   return (
-    <nav className="bg-prussian-blue flex justify-center py-2 w-screen">
+    <nav className="bg-prussian-blue flex justify-center py-2 w-full">
       <ul className="flex items-center justify-center gap-x-2">
         <li className="border border-black rounded-lg">
           <Button
